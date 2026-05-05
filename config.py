bf6ac7a7a95a1381d157d6d45daaff3b9f9fb8ec
backmrpun-hash <backmrpun@gmail.com>
2026-05-06 01:53:11 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+TOKEN = os.getenv("TOKEN")
+FB_CONF = os.getenv("FIREBASE_CONFIG")
+
+DB_URL = "https://bott-54e3e-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+PROMPTPAY_ID = "0655292340"
+
+SETUP_CHANNEL_ID = 1501239836516946061
